4f958c54da1bdbd7c85db7bcdc41f59d30857a0a
EustaceCheng <fbi0258zzz@gmail.com>
2023-04-24 22:58:29 +0800

    add weathercodetostring

--- components/InformationPanel.tsx
+++ components/InformationPanel.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CityPicker from "./CityPicker";
 
 type Props = {
   city: string;
@@ -14,6 +15,30 @@ function InformationPanel({ city, lat, long, results }: Props) {
         <h1 className="text-6xl font-bold">{decodeURI(city)}</h1>
         <p className="text-xs text-gray-400">
           Long/Lat: {long}, {lat}
+        </p>
+      </div>
+      <CityPicker />
+      <hr className="my-10" />
+      <div className="mt-5 flex items-center justify-between space-x-10 mb-5">
+        <div>
+          <p className="text-xl">
+            {new Date().toLocaleDateString("zh-TW", {
+              weekday: "long",
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </p>
+          <p className=" font-extralight">
+            TimeZone: {Intl.DateTimeFormat().resolvedOptions().timeZone}
+          </p>
+        </div>
+        <p className="text-xl font-bold uppercase">
+          {new Date().toLocaleTimeString("zh-TW", {
+            hour: "numeric",
+            minute: "numeric",
+            hour12: false,
+          })}
         </p>
       </div>
     </div>
