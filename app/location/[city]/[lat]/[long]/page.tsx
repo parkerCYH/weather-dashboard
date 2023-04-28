363899486c05fabc1bd4425659fcb46f14ee0a8f
parker cheng <fbi0258zzz@gmail.com>
2023-04-28 16:18:39 +0800

    final

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -6,8 +6,6 @@ import RainChart from "@/components/RainChart";
 import StatCard from "@/components/StatCard";
 import TempChart from "@/components/TempChart";
 import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
-import cleanData from "@/lib/cleanData";
-import getBasePath from "@/lib/getBasePath";
 
 export const revalidate = 60;
 
@@ -31,7 +29,7 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
     },
   });
   const results: Root = data.myQuery;
-  const dataToSend = cleanData(results, city);
+
   return (
     <div className="flex flex-col  min-h-screen md:flex-row ">
       <InformationPanel city={city} lat={lat} long={long} results={results} />
